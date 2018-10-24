e7c78b2ba018ca454c2e71e836a3c34fc3347522
Anaivbvo <42385619+Anaivbvo@users.noreply.github.com>
2018-10-24 09:45:12 -0700

    Initial Upload

--- notes/Hello World.py
+++ notes/Hello World.py
@@ -26,3 +26,30 @@ print("Here is another one...")
 print(6%2)
 print(5%2)
 print(11%4)
+
+
+# Whats is 2^100?
+print(2**100)
+
+# Taking input
+name = input ("What is your name?")
+print("Hello %s." % name)
+
+age = input("How old are you? >_")
+print("%s?!? You belong in a museum." % age)
+print()
+print("%s is really old. They are %s years old." % (name, age))
+
+# Variable Assignments
+car_name = "Anai mobile"
+car_type = "Tesla"
+car_Cylinders = 16
+car_miles_per_gallon = 0.01
+
+#make it print "I have a car callef the Anai mobile. It id a Tesla."
+print(make it print "I have a car callef the Anai mobile. It id a Tesla.)
+
+#Recasting
+real_Age = int(input(How old are you again?))
+hidden_age =  age + 5
+
