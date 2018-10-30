d0e47575c2b6dd7a5fc5d65f5ae6d2b726f9b2a8
Anaivbvo <42385619+Anaivbvo@users.noreply.github.com>
2018-10-30 09:45:35 -0700

    Initial upload

--- notes/Hello World.py
+++ notes/Hello World.py
@@ -1,3 +1,4 @@
+"""
 print("Hello World!")
 
 # This is a comment, This had no effect on the code
@@ -39,6 +40,63 @@ age = input("How old are you? >_")
 print("%s?!? You belong in a museum." % age)
 print()
 print("%s is really old. They are %s years old." % (name,age))
+uhhhh
+uhuhuhu
+hhhhhhhhhh
+hhh
+hhh
+hhh
+hhh
+hhh
+hhh
+hhh
+hhh
+hhh
+hhh
+hhh
+hhh
+"""
+
+"""
+This is a multi line comment
+"""
 
 
+# Functions
+def say_it():
+    print("Hello World!")
 
+
+say_it()
+say_it()
+say_it()
+
+
+# f(x) = 2x + 3
+def f(x):
+    print(2*x + 3)
+
+
+f(1)
+f(5)
+f(5000)
+
+
+# distance formula
+def distance(x1, y1, x2, y2):
+    dist = ((x2-x1)**2 + (y2-y1)**2)**(1/2)
+    print(dist)
+
+
+distance(0, 0, 3, 4)
+distance(0, 0, 5, 12)
+
+# Loops
+for i in range(5) : # This gives the number 9
+    say_it()
+
+for i in range(10) :
+    print(i + 1)
+
+for i in range(5) :
+    f(i)
