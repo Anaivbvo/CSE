d3282deb3784bb58d2dd9615d115e351c393495f
Anaivbvo <42385619+Anaivbvo@users.noreply.github.com>
2018-11-08 09:30:09 -0800

    Initial upload

--- notes/Anai S.L. Lopez - Guess Game.py
+++ notes/Anai S.L. Lopez - Guess Game.py
@@ -10,7 +10,7 @@ a = 3 # A is set up to3
 a == 3 # is equal to 3?
 """
 
+
 print("Guess the number I am thinking of!")
 print("This number is between 1 - 10.")
 print("You have 5 chances! I'll help you along the way!")
-
