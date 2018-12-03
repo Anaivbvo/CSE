88c6dd304a6ceb5071d50c89d7cafd2a8eb336c5
Anaivbvo <42385619+Anaivbvo@users.noreply.github.com>
2018-12-03 09:06:06 -0800

    Initial upload

---
+++ notes/Challanges.py
@@ -0,0 +1,13 @@
+# Easy:
+# 1.	Write a Python program which accepts the user's first and last name and print them in reverse order with a
+# space between them.
+
+first name = print("What is your first name? ")
+last name = print("What is your last name? ")
+
+# 2.	Write a Python program to find whether a given number (accept from the user) is even or odd, print out an
+# appropriate message to the user.
+
+# 3.	Write a Python program that will accept the base and height of a triangle and compute the area.
+
+# 4.	Write a Python program to check if a number is positive, negative or zero.
