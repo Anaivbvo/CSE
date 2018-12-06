ede0cce68dcc461598de4efab614ac0baa6858a4
Anaivbvo <42385619+Anaivbvo@users.noreply.github.com>
2018-12-06 09:44:33 -0800

    Initial upload

--- notes/Anai S.L. Lopez - Fix it.py
+++ notes/Anai S.L. Lopez - Fix it.py
@@ -9,27 +9,30 @@ print("So, you're %s old, %s tall and %s heavy." % (age, height, weight))
 
 print('Let\'s practice everything.')
 print('You\'d need to know \'bout escapes')
-    with \\ that do \n newlines and \t tabs.')
+print("with \\ that do \n newlines and \t tabs.")
 
-poem = "\tThe lovely world
+poem = """
+\tThe lovely world
 with logic so firmly planted
 cannot discern \n the needs of love
 nor comprehend passion from intuition
 and requires an explanation
-\n\t\twhere there is none."
+\n\t\ttwhere there is none.
+"""
 
 print("--------------")
 print(poem)
 print("--------------")
 
 
-five = (10 - 2 + 3 )
+five = (10 - 2 + (-3))
 print("This should be five: %s" % five)
 
-def secret_formula(started)
+def secret_formula(started):
+
     jelly_beans = started * 500
     jars = jelly_beans / 1000
-    crates = jars  100
+    crates = jars / 100
     return jelly_beans, jars, crates
 
 
@@ -44,16 +47,13 @@ print(f"We'd have {beans} beans, {jars} jars, and {crates} crates.")
 start_point = start_point / 10
 
 print("We can also do that this way:")
-formula = secret_formula(startpoint)
+formula = secret_formula(start_point)
 # this is an easy way to apply a list to a format string
 print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
-
-
 
 people = 20
 cats = 30
 dogs = 15
-
 
 if people < cats:
     print("Too many cats! The world is doomed!")
@@ -67,7 +67,6 @@ if people < dogs:
 if people > dogs:
     print("The world is dry!")
 
-
 dogs = +5
 
 if people >= dogs:
@@ -75,7 +74,6 @@ if people >= dogs:
 
 if people <= dogs:
     print("People are less than or equal to dogs.")
-
 
 if people == dogs:
     print("People are dogs.")
