41d34167598e44fd12d9216c07823b3e2c86f0d4
Anaivbvo <42385619+Anaivbvo@users.noreply.github.com>
2018-12-07 10:35:20 -0800

    Initial upload

--- notes/Challanges.py
+++ notes/Challanges.py
@@ -1,44 +1,7 @@
-# Easy
-
 """
-# 1.	Write a Python program which accepts the user's first and last name and print them in reverse order with a
-# space between them.
-"""
-def challenge1 (firstname, lastname):
-
-"""
- # 2. Write a Python program to find whether a given number (accept from the user) is even or odd, print out an
-# appropriate message to the user.
-"""
-
-
-
-"""
-# 3.	Write a Python program that will accept the base and height of a triangle and compute the area.
-"""
-
-"""
-# 4.	Write a Python program to check if a number is positive, negative or zero.
-"""
-
-"""
-Medium:
-5.	Write a Python program which accepts the radius of a circle from the user and compute the area.
-Sample Output : 
-r = 1.1
-Area = 3.8013271108436504
-
-6.	Write a Python program to get the volume of a sphere from a given radius.
-
-7.	Write a Python program that accepts an integer (n) and computes the value of n+nn+nnn.
-
-8.	Write a Python program to test whether a number is within 150 of 2000 or 3000.
-"""
-
-"""
-Hard:
-9.	Write a Python program to test whether a passed letter is a vowel or not.
-10.	Write a Python program to check if a string is numeric
-11.	Write a Python program to get the system time
-12.	Write a Python program to find common divisors between two numbers in a given pair
+Easy:
+1.	Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them.
+2.	Write a Python program to find whether a given number (accept from the user) is even or odd, print out an appropriate message to the user.
+3.	Write a Python program that will accept the base and height of a triangle and compute the area.
+4.	Write a Python program to check if a number is positive, negative or zero.
 """
