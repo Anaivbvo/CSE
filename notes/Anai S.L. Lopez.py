7847aaf0ce82ec08b726b393ea05c15a491b787c
Anaivbvo <vbvo@wetigers.org>
2019-01-18 10:12:03 -0800

    Initial upload

--- notes/Anai S.L. Lopez.py
+++ notes/Anai S.L. Lopez.py
@@ -2,5 +2,10 @@ import random
 bank = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
 guesses = 8
 
-word = list(random.choices(bank))
-
+word = random.choices(bank)
+print(word)
+word2 = list(word)
+print(word2)
+print(len(word2))
+word3 = word.split(' ')
+print(word3)
