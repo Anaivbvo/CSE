b515b2aa42b9bd7c8b10fe003c0f419fa012427c
Anaivbvo <vbvo@wetigers.org>
2019-02-08 10:26:00 -0800

    initial upload

--- notes/word tries.py
+++ notes/word tries.py
@@ -90,67 +90,89 @@ main_map = {
 
         },
         'KITCHEN': {
-            'NAME': "",
-            'DESCRIPTION': "",
-            'PATHS': {}
+            'NAME': "Cafeteria_Kitchen",
+            'DESCRIPTION': "It's the cafeteria's kitchen. They never cook food, so no one is ever sure why there is so "
+                           "much ingredients here... Go WEST to go back into the Food_Booths.",
+            'PATHS': {
+                'WEST': 'Food_Booths'
+            }
         }
     },
     'MUSIC_BUILDING': {
         'ENTRANCE': {
-            'NAME': "",
-            'DESCRIPTION': "",
-            'PATHS': {}
+            'NAME': "The_Band_Room",
+            'DESCRIPTION': "It's the band room! This is the home of the band kids and the color guard members! The "
+                           "entrance is maybe the best part of the whole room! There is a fridge here where kids leave "
+                           "their drinks and snacks. No one knows who first brought the fridge, but in fear of losing "
+                           "it, no one ha asked the director as well. There is also a lost and found box here. Shoes "
+                           "and music sheets are often found here. Sometimes even reed cases. Going NORTH is the "
+                           "Office of the director. Going SOUTH is the Drum-Line_Lockers. Going WEST is the "
+                           "Main_Band_Room.",
+            'PATHS': {
+                'NORTH': 'Office',
+                'SOUTH': 'Drum-Line_Lockers',
+                'WEST': 'Main_Band_Room'
+            }
 
         },
         'OFFICE': {
+            'NAME': "Office",
+            'DESCRIPTION': "It's the director's office! Only section leaders and drum majors enter here, as well as the"
+                           "band instructors. There's music, trophies, and.. dvd cases? Going SOUTH_WEST leads you into"
+                           " the Main_Band_Room. Going SOUTH_EAST leads you back to the The_Band_Room entrance.",
+            'PATHS': {
+                'SOUTH_EAST': 'The_Band_Room',
+                'SOUTH_WEST': 'Main_Band_Room'
+            }
+
+        },
+        'DRUM-LINE': {
+            'NAME': "",
+            'DESCRIPTION': "IT IS THE DRUM LINE LOCKER ROOM. Only god tier level players dare come in here. Unless you're"
+                           "a percussion player, a member of the drum line, or a god tier player, you best leave this godly "
+                           "place. Going NORTH leads you into The_Band_Room entrance. Going SOUTH_WEST leads you to the "
+                           "Percussion_Lockers. Going WEST leads you into the Main_Band_Room. Going NORTH leads you back into "
+                           "The_Band_Room entrance.",
+            'PATHS': {}
+
+        },
+        'PERCUSSION': {
             'NAME': "",
             'DESCRIPTION': "",
             'PATHS': {}
 
         },
-        'DRUM-LINE_LOCKERS': {
+        'WOOD-WIND': {
             'NAME': "",
             'DESCRIPTION': "",
             'PATHS': {}
 
         },
-        'PERCUSSION_LOCKERS': {
+        'MAIN_ROOM': {
             'NAME': "",
             'DESCRIPTION': "",
             'PATHS': {}
 
         },
-        'WOOD-WIND_LOCKERS': {
+        'STORAGE_ROOM': {
             'NAME': "",
             'DESCRIPTION': "",
             'PATHS': {}
 
         },
-        'MAIN_BAND_ROOM': {
+        'ORCHESTRA': {
             'NAME': "",
             'DESCRIPTION': "",
             'PATHS': {}
 
         },
-        'UNIFORM_STORAGE_ROOM': {
+        'BRASS': {
             'NAME': "",
             'DESCRIPTION': "",
             'PATHS': {}
 
         },
-        'ORCHESTRA_ROOM': {
-            'NAME': "",
-            'DESCRIPTION': "",
-            'PATHS': {}
-
-        },
-        'BRASS_LOCKERS': {
-            'NAME': "",
-            'DESCRIPTION': "",
-            'PATHS': {}
-
-        },
-        'COLOR-GUARD_LOCKERS': {
+        'COLOR-GUARD': {
             'NAME': "",
             'DESCRIPTION': "",
             'PATHS': {}
--- notes/world.py
+++ notes/world.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 main_map = {
     'OUTSIDE': {
         'OUTER_STAGE_TABLE': {
@@ -149,38 +148,3 @@ main_map = {
         },
     }
 }
-=======
-main_map = {}
-
-print("-"*12)
-print(" ")
-print("Try this out: Basic tips: ")
-print("To quit or end the game just type: q, quit, or exit.")
-print(" ")
-print("-"*12)
-print(" ")
-
-# Controller
-playing = True
-current_node = main_map['Table']
-directions = ['NORTH', 'SOUTH', 'EAST', 'WEST', 'NORTH_EAST', 'NORTH_WEST']
-
-while playing:
-    print(current_node['NAME'])
-    print(current_node['DESCRIPTION'])
-    command = input(">_")
-    if command.lower() in ['q', 'quit', 'exit']:
-        playing = False
-    elif command.upper in directions:
-        try:
-            room_name = current_node['PATHS'][command.upper()]
-            current_node = main_map[room_name]
-        except KeyError:
-            print("I can't go that way.")
-        except AttributeError:
-            pass
-        except ArithmeticError:
-            pass
-    else:
-        print("Command not found")
->>>>>>> 081cc91c84a764e6bce301574fc7185d9acdce20
