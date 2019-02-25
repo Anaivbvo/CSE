4e492abb22b61b84864c8b2720b080b6579d3f27
Anaivbvo <vbvo@wetigers.org>
2019-02-25 09:13:35 -0800

    initial upload

--- notes/Anai S.L. Lopez - Object.py
+++ notes/Anai S.L. Lopez - Object.py
@@ -1,9 +1,10 @@
 class NoteBook(object):
-    def __init__(self, cover_title):
+    def __init__(self, cover_title, color):
         self.pages = 4
         self.cover = True
         self.cover_title = cover_title
-        self.entries = True
+        self.color = color
+        self.closed = True
 
     def rip_page(self):
         print("RIP!!")
@@ -11,14 +12,14 @@ class NoteBook(object):
         self.pages = self.pages - 1
         print(self.pages)
 
-    def write_entry(self):
+    def write_entry(self,):
         if self.pages == 0:
             print("You can't write an entry.")
             print("You have no more pages to write on.")
             return
         elif self.pages > 0:
-            input("Write your entry: ")
-            print(input)
+            entry = input("Write your entry: ")
+            print(entry)
             return
 
     def rip_cover(self):
@@ -26,7 +27,16 @@ class NoteBook(object):
         print("You have ripped the cover of your note book.")
         self.cover = False
 
+    def open_book(self):
+        command = input("Open book? ")
+        if command.lower() in ["no"]:
+            self.closed = True
+        elif command.upper() in ["yes"]:
+            self.closed = False
+            print("You have open the book!")
 
-my_book = NoteBook("Book1")
 
-my_book.write_entry()
+my_book = NoteBook("Book1", "Blue")
+
+my_book.rip_page()
+my_book.open_book()
