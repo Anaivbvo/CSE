b29f4e3f29bf9323d2361a64153dee2ac34e29dd
Anaivbvo <42385619+Anaivbvo@users.noreply.github.com>
2019-03-04 20:12:03 -0800

    initial upload

--- notes/Anai S.L. Lopez - Characters.py
+++ notes/Anai S.L. Lopez - Characters.py
@@ -10,22 +10,19 @@ class Teacher(object):
         print(Director.description)
         print("How do you greet him? cheerful? tired? Or neutral?")
         answer = input(">_")
+        print("You greeted the Director in a ", answer, "manner.")
+        print("DIRECTOR:")
         if answer.upper() == greetings[0]:
-            print("You cheerfully greeted the director!")
-            print("DIRECTOR:")
             print("Morning! Hope you're ready for a productive day today! Do your best on the field today!")
         elif answer.upper() == greetings[1]:
-            print("You greeted the director in a tired manner.")
-            print("DIRECTOR:")
             print("Not enough rest? Make sure to step out of practice if you're not feeling well. Get some water too"
                   " when you need it.")
         elif answer.upper() == greetings[2]:
-            print("You greeted the director with little to no visible emotion.")
-            print("DIRECTOR")
             print("Oh? Where's that enthusiasm? C'mon get some water and get ready for practice like you've never done"
                   " before!")
         else:
             print("That's not a greeting!")
+            print("Well... the director is gone now..")
 
 
 class SectionLeader(object):
@@ -38,24 +35,27 @@ class SectionLeader(object):
         print("There's your section leader!")
         print(Sax_Section_Leader.name)
         print(Sax_Section_Leader.description)
-        print("How do you greet him?")
+        print("How do you greet him? happy? sad? or avoid?")
         answer = input(">_")
         print("You greeted your section leader in a", answer, "manner.")
         print("SECTION LEADER: ")
-        if answer == greetings2[0]:
+        if answer.upper() == greetings2[0]:
             print("'Oh hi there. Good to see you're doing well. Get your stuff ready we're heading out to the field "
                   "soon'")
             print("...")
             print("Hm.. That's probably the nicest they'll be to you for now.")
-        elif answer == greetings2[1]:
+        elif answer.upper() == greetings2[1]:
             print("'Uh, is something wrong? I'm bad at talking to newbies so if something is wrong step out of practice"
                   "when you need to.")
             print("...")
             print("How nice of them!")
-        elif answer == greetings2[2]:
+        elif answer.upper() == greetings2[2]:
             print("'...'")
             print("...")
             print("Hm,, you made it a bit too obvious..")
+        else:
+            print("'...?'")
+            print("I don't know what you were trying to say and nor either did your section leader...")
 
 
 Director = Teacher("Band Director", "A very nice, funny, and welcoming man. Very talented when it comes to music and "
@@ -67,16 +67,3 @@ Director.talk_to()
 print("You walked away from your director..")
 print("Oh who's that?")
 Sax_Section_Leader.talk_to()
-
-"""
-if answer == greetings2[0]:
-    print("You happily greeted your section leader!")
-    print("SECTION LEADER:")
-    print("'Oh hi there. Good to see you're doing well. Get your stuff ready we're heading out to the field "
-          "soon'")
-    print("...")
-    print("Hm.. That's probably the nicest they'll be to you for now.")
-elif answer == greetings2[1]:
-    print("You greeted your section leader in a sad manner..")
-    print("SECTION LEADER")
-"""
