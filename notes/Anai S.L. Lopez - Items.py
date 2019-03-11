29aeae79dbf39b5931cb4320d5f03f0750f740a0
Anaivbvo <vbvo@wetigers.org>
2019-03-11 09:43:24 -0700

    initial upload

--- notes/Anai S.L. Lopez - Items.py
+++ notes/Anai S.L. Lopez - Items.py
@@ -1,24 +1,17 @@
+# Player
 class Player(object):
-    def __init__(self, name, health=100, social=10, hydration=100, ):
+    def __init__(self, name):
         self.name = name
-        self.health = health
-        self.social = social
-        self.hydration = hydration
+        self.health = 0
+        self.social = 10
+        self.hydration = 20
         self.inventory = []
-
-    def name(self):
-        name = input("Helli, what is your name? >_ ")
-        Player.name = name
 
     def health_gained(self, amount):
         self.health += amount
 
-
-class Consumable(Item):
-
-    def __init__(self, name, health):
-        super(Consumable, self).__init__(name)
-        self.health = health
+    def hydration_gained(self, amount1):
+        self.hydration += amount1
 
     def eat(self):
         print("Do you want to eat this item?")
@@ -32,12 +25,65 @@ class Consumable(Item):
             print("You have decided to not eat the ", Consumable, ".")
 
 
+# Consumable
+class Consumable(object):
+    def __init__(self, name, health, hydration):
+        super(Consumable, self).__init__(name)
+        self.health = health
+        self.hydration = hydration
+
+
+# Solids
+class Apples(Consumable):
+    def __init__(self, name, description):
+        super(Apples, self).__init__(name, health=12, hydration=2)
+        self.name = name
+        self.description = description
+        self.amount = Player.health + 12
+        self.amount1 = Player.hydration + 2
+
+
+class Gum(Consumable):
+    def __init__(self, name, description):
+        super(Gum, self).__init__(name, health=0, hydration=0)
+        self.name = name
+        self.description = description
+        self.amount = Player.health + 0
+        self.amount1 = Player.hydration + 0
+
+
 class Noodles(Consumable):
     def __init__(self, name, description):
-        super(Noodles, self).__init__(name, health=10)
+        super(Noodles, self).__init__(name, health=10, hydration=4)
         self.name = name
         self.description = description
         self.amount = Player.health + 10
+        self.amount1 = Player.hydration + 4
 
 
-ME = Player()
+class Fries(Consumable):
+    def __init__(self, name, description):
+        super(Fries, self).__init__(name, health=8, hydration=-5)
+        self.name = name
+        self.description = description
+        self.amount = Player.health + 4
+        self.amount1 = Player.hydration - 5
+
+
+# Liquids
+class FreshWater(Consumable):
+    def __init__(self, name, description):
+        super(FreshWater, self).__init__(name, health=0, hydration=15)
+        self.name = name
+        self.description = description
+        self.amount = Player.health + 0
+        self.amount1 = Player.hydration + 15
+
+
+class FountainWater(Consumable):
+    def __init__(self, name, description):
+        super(FountainWater, self).__init__(name, health=0, hydration=5)
+        self.name = name
+        self.description = description
+        self.amount = Player.health + 0
+        self.amount1 = Player.hydration + 5
