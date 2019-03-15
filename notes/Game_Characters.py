20aff8e6271859913c8e0cc6578b513ec34e11be
Anaivbvo <vbvo@wetigers.org>
2019-03-15 10:38:22 -0700

    initial upload

--- notes/Game_Characters.py
+++ notes/Game_Characters.py
@@ -1,7 +1,7 @@
 import random
 
 
-# Player
+# Person
 class Person(object):
     def __init__(self, name, description, rank, social):
         self.name = name
@@ -18,42 +18,46 @@ class Character(object):
         self.description = description
         self.rank = rank
         self.inventory = []
-        self.want = []
+        self.want = random.choices(Person.inventory)
         self.area = area
 
     def trade(self):
-        print("((What would like from ", self.name, "?))")
+        print("((What would you like from ", self.name, "?))")
         print(self.inventory)
         wanted_item = input(">_")
         if wanted_item in self.inventory:
-            character_want = random.choice(Player.inventory)
-            print("'Alright I see, well in return I would like ", character_want, ".'")
+            print("'Alright I see, well in return I would like ", self.want, ".'")
             print("((Do you have the item?))")
-            if character_want in Player.inventory:
-                print(Player.inventory)
+            if character_want in Person.inventory:
+                print(Person.inventory)
                 print("((You do have the item, trade?))")
-                yes = ['YES']
-                no = ['NO']
                 answer = input(">_")
-                if answer.upper() in yes:
+                if answer.upper() in ['YES']:
                     print(self.name, ":")
                     print("'Awesome! Alright then looks like you got a deal!'")
-                    Player.inventory.remove(character_want)
+                    Person.inventory.remove(character_want)
                     self.inventory.append(character_want)
-                    Player.inventory.append(wanted_item)
+                    Person.inventory.append(wanted_item)
                     self.inventory.remove(wanted_item)
                     print("((Awesome! You know have ", wanted_item, " but lost ", character_want, ".")
-                    Player.social += 5
+                    Person.social += 5
                     print("((You gained plus 5 on your social level!")
-                elif answer.upper() in no:
+                elif answer.upper() in ['NO']:
                     print(self.name, ":")
                     print("'Aw that's too bad, well maybe someone else might have the item for a different price.")
                     print("'Come back to me when you're ready for a good deal1'")
-            elif character_want not in Player.inventory:
-                print(Player.inventory)
+            elif character_want not in Person.inventory:
+                print(Person.inventory)
                 print("((Hmm.. Seems like you don't have the item...))")
                 print(self.name, ":")
                 print("'Oh well... I'm sure someone else might be able to trade ", wanted_item, "for something ya got.")
         elif wanted_item not in self.inventory:
             print(self.name, ":")
             print("'Sorry pal, don't have that on me right now. You can try asking others.")
+
+
+me = Person("me", "it's me", "freshmen", 10)
+me.inventory = ["hat", "cookie", "recorder"]
+upperclassmen = Character("sophomore", "2nd year", "sophomore", "band_tree")
+upperclassmen.inventory = ["sax_reed", "water", "bread"]
+upperclassmen.trade()
