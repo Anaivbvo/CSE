b52c0797164e553ff174bdd168b58f342f8b32a6
Anaivbvo <vbvo@wetigers.org>
2019-03-25 09:44:38 -0700

    initial upload

--- notes/Game_Characters.py
+++ notes/Game_Characters.py
@@ -1,4 +1,6 @@
 import random
+import Game_Items
+
 # Player
 
 
@@ -12,9 +14,6 @@ class Player(object):
         self.social = social
         self.inventory = []
 
-
-me = Player("me", "it's me", "freshmen", 10, 10, 10)
-me.inventory = ['fries', 'noodles', '2nd chair music', 'water', 'pencil']
 
 # Characters
 
@@ -67,31 +66,3 @@ class Characters(object):
                 return self.trade()
             elif answer.upper() in ['NO']:
                 print(self.sorry, "you're better off asking someone else..")
-
-
-print("The director has given you a mission to find blank paper, but you don't have any on you at the moment.")
-print("determined to impress the director you ask around your section!")
-print("oh! there's a member!")
-print("-"*10)
-Nathaniel = Characters("Nathaniel", "he's part of ur section", "sophomore", "sorry pal,", "Cool!")
-print(Nathaniel.name, "-", Nathaniel.description, "-", Nathaniel.rank)
-print("You ask Nathaniel in search for paper!")
-Nathaniel.inventory = ['sunglasses', '1st chair music', 'spicy noodles', 'energy drink', 'chocolate']
-Nathaniel.trade()
-print("-"*10)
-print("hmm... you go ask someone else..")
-print("oh! there's someone else!")
-print("-"*10)
-Sergio = Characters("Sergio", "sophomore symphonic section leader Sergio", "sophomore", "yeah uh,", "Okay,")
-print(Sergio.name, "-", Sergio.description, "-", Sergio.rank)
-print("You ask Sergio in search for paper!")
-Sergio.inventory = ['alto reed 2.5', 'sports drink', 'empty folder', '1st chair music', 'chips']
-Sergio.trade()
-print("-"*10)
-print("you go off still in search for paper!. seems like neither of the sophomore have that..")
-print("hey there's someone else!")
-Cody = Characters("Cody", "next year section leader Cody~", "junior", "sorry buddy,", "Nice!")
-print(Cody.name, "-", Cody.description, "-", Cody.rank)
-print("You ask Cody in search for paper!")
-Cody.inventory = ['water', 'alto reed 3.5', 'neck strap', 'cap', 'flip book']
-Cody.trade()
