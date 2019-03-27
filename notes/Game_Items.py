865ba6560f47f2ec396398db4cc9d5ab245d7021
Anaivbvo <vbvo@wetigers.org>
2019-03-27 09:43:17 -0700

    food items

--- notes/Game_Items.py
+++ notes/Game_Items.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Player(object):
     def __init__(self, health_level, hydration_level, social_level):
         self.health_level = health_level
@@ -8,11 +11,19 @@ class Player(object):
 me = Player(10, 10, 10)
 
 
-class ConsumableLiquid(object):
-    def __init__(self, name, description, hydration_plus):
+class Items(object):
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+
+class ConsumableLiquid(Items):
+    def __init__(self, name, description, hydration_plus, social_plus):
+        super(ConsumableLiquid, self).__init__(name, description)
         self.name = name
         self.description = description
         self.hydration_plus = hydration_plus
+        self.social_plus = social_plus
 
     def drink(self):
         print("Would you like to drink:", self.name, "?")
@@ -27,11 +38,13 @@ class ConsumableLiquid(object):
             self.drink()
 
 
-class ConsumableSolid(object):
-    def __init__(self, name, description, health_plus):
+class ConsumableSolid(Items):
+    def __init__(self, name, description, health_plus, social_plus):
+        super(ConsumableSolid, self).__init__(name, description)
         self.name = name
         self.description = description
         self.health_plus = health_plus
+        self.social_plus = social_plus
 
     def eat(self):
         print("Would you like to eat: ", self.name, "?")
@@ -46,21 +59,55 @@ class ConsumableSolid(object):
             self.eat()
 
 
-class Items(object):
-    def __init__(self, name, description, social_plus):
+class HealthItems(Items):
+    def __init__(self, name, description, health_plus):
+        super(HealthItems, self).__init__(name, description)
         self.name = name
         self.description = description
+        self.health_plus = health_plus
+
+    def use(self):
+        print("Would you like to use the item?")
+        answer = input(">_")
+        if answer in ['yes']:
+            print("you used the item: ", self.name)
+            me.health_level += self.health_plus
+            print("you've gained:", self.health_plus, "in your health level!")
+
+
+class Music(Items):
+    def __init__(self, name, description, section, chair, social_plus):
+        super(Music, self).__init__(name, description)
+        self.name = name
+        self.description = description
+        self.section = section
+        self.section = section
+        self.chair = chair
         self.social_plus = social_plus
 
-    def when_trade(self):
-        print("You've gained:", self.social_plus, "on your social level!")
-        me.social_level += self.social_plus
 
-
-Water = ConsumableLiquid("Water", "Fresh cold water! perfect for a hot day! a plus 20 on your hydration level!", 20)
-Milk = ConsumableLiquid("Milk", "School milk. It's cold too.", -1)
-Arizona = ConsumableLiquid("Arizona", "nayeli", 5)
-GrossSamwitch = ConsumableSolid("sandwich", "a chicken sandwich from the school food.", -5)
-print("%s: %s" % (Water.name, Water.description))
-Water.drink()
-GrossSamwitch.eat()
+Water = ConsumableLiquid("Water", "Fresh and cold water you brought from home! It even has ice!", 15, 5)
+Milk = ConsumableLiquid("Milk", "Cold school plain milk. Nothing special about it.", 7, 2)
+SportsDrink = ConsumableL4("Sports Drink", "This is the usual brand of sports drink everyone brings to camp. "
+                                           "Refreshing, fruity, and energy giving!", 10, 15)
+Arizona = ConsumableLiquid("Arizona", "This is most everyone's favorite juice. This can most definitely bring up your "
+                                      "social level, though how sweet it is, it may not be as nice with your health "
+                                      "level.", 4, 8)
+Rebel = ConsumableLiquid("Rebel Energy Drink", "Every one loves this energy drink! Though it's not as healthy as water,"
+                                               " everyone craves for this after practice.", 2, 15)
+Apple = ConsumableSolid("Apple", "A nice red apple! Sweet but solid! Though everyone likes junk food, no one could "
+                                 "decline a nice apple!", 10, 5)
+Fries = ConsumableSolid("Mac's Fries", "Everyone loves fried! Though it'snot the healthiest item, but that doesnt stop"
+                                       " anyone from eating them!", 10, 15)
+Burger = ConsumableSolid("Mac's Burger", "A cheese burger! If you'd compare this to Mac's Fries, you'd think the "
+                                         "burger was the side option. Though this is a whole meal for anyone after "
+                                         "practice!", 8, 12)
+Sandwich = ConsumableSolid("School Sandwich", "Since this is a school food, you never know if this sandwich is any "
+                                              "good or bad...", random.randint(-20, 20), random.randint(-5, 5))
+Salad = ConsumableSolid("Salad", "School's salad. This is a surprise whether you feel better or worse after eating "
+                                 "this...", random.randint(-10, 10), random.randint(-5, 5))
+SunScreen = HealthItems()
+EnergyShot = HealthItems()
+Alto1A = Music()
+Alto1B = Music()
+Alto2A = Music()
